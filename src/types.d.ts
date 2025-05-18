f533cd6997fa7465960d9242982607c0e904fd78
ato <ato@ato-NM1.local>
2025-05-18 22:24:11 +0900

    Updates

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-// react-image-gallery에 대한 타입 선언
-declare module 'react-image-gallery';
-
-// styled-jsx에 대한 타입 선언
-declare namespace JSX {
-  interface IntrinsicAttributes {
-    jsx?: boolean;
-  }
-} 
